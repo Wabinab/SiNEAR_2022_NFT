c975cde62824e7d4b2e9b877edf940033f220980
Ubuntu <azureuser@railsvm.hiqpufyakhnehggv4ozcka3wmb.bx.internal.cloudapp.net>
2022-04-24 08:31:12 +0000

    production based puma

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,11 +2,14 @@ class CardsController < ApplicationController
 
   def new
     @card = Card.new
-    @token_id = "f_nft_" + Time.now.to_f.to_s.gsub('.', '_')
+    @token_id = "f_nft_" + Time.now.to_f.to_s.gsub('.', '_') + '_' + ('a'..'z').to_a.shuffle[0, 5].join
   end
 
+  # Index test fail, because params[:token_id] is undefined. 
+  # We didn't create a database, so this doesn't work anyways! 
+  # In the future, we might want to remove it, though. 
   def index  
-    @card = Card.find_by(token_id: card_params[:token_id]);
+    @card = Card.find_by(token_id: params[:token_id]);
     redirect_to @card
   end
 
@@ -17,12 +20,12 @@ class CardsController < ApplicationController
   # def create
   # end
 
-  private
-    def card_params 
-      params.require(:card).permit(:token_id)
-    end
+  # private
+    # def card_params 
+    #   params.require(:card).permit(:token_id)
+    # end
 
-    def token_id 
-      params.require(:token_id)
-    end
+    # def token_id 
+    #   params.require(:token_id)
+    # end
 end
